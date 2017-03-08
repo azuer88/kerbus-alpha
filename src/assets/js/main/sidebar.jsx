9d298c23c4853851c814c1322d1d3def5443f32a
Blue Cuenca <blue.cuenca@ymail.com>
2017-03-08 09:24:51 +0800

    add alert onClicked event
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/assets/js/main/sidebar.jsx
+++ src/assets/js/main/sidebar.jsx
@@ -3,13 +3,17 @@
  */
 var React = require('react');
 
+function mainmenu_click() {
+   alert('please select an item below');
+}
+
 module.exports = React.createClass({
     render: function() {
         return (
             <div id="sidebar-wrapper">
                <ul className="sidebar-nav">
                    <li className="sidebar-brand">
-                      <a href="#">
+                      <a href="#" onClick={mainmenu_click}>
                       Main Menu
                       </a>
                    </li>
