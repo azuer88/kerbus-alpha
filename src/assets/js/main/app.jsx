3e90e28ba36c9c3b7ccd96f0def4f28bbecf3cf9
Blue Cuenca <blue.cuenca@ymail.com>
2017-03-20 11:59:21 +0800

    refactor menu component
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/assets/js/main/app.jsx
+++ src/assets/js/main/app.jsx
@@ -1,47 +1,14 @@
 var React = require('react');
 
 var MyJumbotron = require('react-bootstrap/lib/Jumbotron');
-var Sidebar = require('./sidebar');
-var MenuItem = require('./menuitem');
-
-var fetcher = require('fetch-er');
-var Holder = require('react-placeholder');
+var DynMenu = require('./dymenu');
 
 module.exports = React.createClass({
-    getInitialState: function() {
-         return {
-             menuready: false,
-             menu: []
-         } 
-    },
-    
-    componentDidMount: function() {
-       fetcher.getJSON('/menuitem/?format=json').then(
-         ([v, s, r]) => {
-           const menu = v.results;
-           this.setState({ menu: menu, menuready: true });
-       });
-    },
-
     render: function() {
         return (
             <div>
                 <div id="sidebar-wrapper">
-                    <Holder
-                       rows={10}
-                       ready={this.state.menuready}
-                       className="sidebar-brand"
-                    >
-                       <Sidebar>
-                          {
-                            this.state.menu.map(item =>
-                               <MenuItem key={item.id} href={item.link}>
-                                   {item.title}
-                               </MenuItem>
-                            )
-                          }
-                       </Sidebar>
-                    </Holder>
+                   <DynMenu />
                 </div>
                 <div id="page-content-wrapper">
                         <MyJumbotron>
