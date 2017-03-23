aa8d047de4e237df36fa053727450d7b711cd3f3
Blue Cuenca <blue.cuenca@ymail.com>
2017-03-23 13:15:54 +0800

    remove other api views
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/kerbus/asimplemenu/urls.py
+++ src/kerbus/asimplemenu/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import url, include
-from rest_framework import routers
+from django.conf.urls import url
+# from django.conf.urls import include
+# from rest_framework import routers
 
-from views import MenuItemViewSet
+# from views import MenuItemViewSet
 # from views import MenuGroupViewSet
 from views import MenuItemList
 
-router = routers.DefaultRouter()
-router.register(r'item', MenuItemViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'item', MenuItemViewSet)
 # router.register(r'group', MenuGroupViewSet)
 
 urlpatterns = [
-    url('^', include(router.urls)),
+    # url('^', include(router.urls)),
     url('^group/(?P<group>.+)/$', MenuItemList.as_view()),
 ]
