f2c2fa5ff47e02e0c0f0ac355d307774a03a3b63
Blue Cuenca <blue.cuenca@ymail.com>
2017-03-30 10:47:16 +0800

    add app to handle login
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

---
+++ src/kerbus/account/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+from django.contrib.auth import authenticate, login
+
+def login_form(request, error=None):
+    pass
+
+def authenticate_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        next_page = request.get('next', '/')
+        return redirect(next_page)
+    else:
+        reuturn login_form(request, error="Invalid user name or password")
+
+def index(request):
+    if request.method == 'POST':
+        return authenticate_user(request)
+    else:
+        return login_form(request)
+
+
