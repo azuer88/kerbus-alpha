fffbec10e3a9222676b87ced5e28a816e79c1322
Blue Cuenca <blue.cuenca@ymail.com>
2017-04-05 14:36:25 +0800

    add permission_classes
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/kerbus/snippet/views.py
+++ src/kerbus/snippet/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from snippet.models import Snippet
 from snippet.serializers import SnippetSerializer
@@ -7,8 +8,10 @@ from snippet.serializers import SnippetSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticated,)
