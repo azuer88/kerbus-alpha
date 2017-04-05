315e673cec1e0d17786dee7b2eccc61cca8bd60c
Blue Cuenca <blue.cuenca@ymail.com>
2017-04-05 08:53:45 +0800

    use rest api decorators and utility functions
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/kerbus/kerbus/settings.py
+++ src/kerbus/kerbus/settings.py
@@ -165,10 +165,6 @@ except NameError:
         pass
 
 REST_FRAMEWORK = {
-    # Use Django's standard django.contrib.auth permissions
-    'DEFAULT_PERMISSION_CLASSES': [
-         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
