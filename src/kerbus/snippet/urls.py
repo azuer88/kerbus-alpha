20502c5236aeb9b53a186978314c25fb3f55a03e
Blue Cuenca <blue.cuenca@ymail.com>
2017-04-05 13:44:10 +0800

    change to class-based views
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/kerbus/snippet/urls.py
+++ src/kerbus/snippet/urls.py
@@ -4,8 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import views
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail, name="snippet_detail"),
-    url(r'^', views.snippet_list, name="snippet_list"),
+    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(),
+        name="snippet_detail"),
+    url(r'^', views.SnippetList.as_view(),
+        name="snippet_list"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
