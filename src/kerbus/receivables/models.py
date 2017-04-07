64c44a12f7e5eb9c1a16069d6baba85ef1580479
Blue Cuenca <blue.cuenca@ymail.com>
2017-04-07 13:34:00 +0800

    add person class with all util function and mixin
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- src/kerbus/receivables/models.py
+++ src/kerbus/receivables/models.py
@@ -2,4 +2,32 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+def name_field(**kargs):
+    kargs.setdefault('max_length', 60)
+    kargs.setdefault('blank', False)
+    kargs.setdefault('default', '')
+    kargs.setdefault('null', False)
+    return models.CharField(**kargs)
+
+
+class PersonMixin(models.Model):
+    first_name = name_field()
+    last_name = name_field
+    midde_name = name_field(blank=True)
+
+    @property
+    def full_name(self):
+        if self.middle_name:
+            return "%s, %s %s." % (self.last_name,
+                                   self.first_name,
+                                   self.midde_name)
+        else:
+            return "%s, %s" % (self.last_name, self.first_name)
+
+    class Meta:
+        abstract = True
+
+
+class Person(PersonMixin, models.Model):
+    pass
